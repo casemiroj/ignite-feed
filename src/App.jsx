25af23a4d2c3f1db0e4f6349b30b19d23b59c77a
João Casemiro <jv.casemiro@icloud.com>
2022-06-27 20:04:51 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import './styles.css'
 
 export function App() {
   return (
-    <>
+    <div>
+      <Header />
       <Post 
         author="João Casemiro" 
         content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti distinctio beatae ducimus, recusandae eum impedit dicta earum doloremque, culpa at saepe voluptatem? Ipsum dolor dolores, temporibus sunt officiis accusantium quod?"
@@ -11,6 +14,6 @@ export function App() {
         author="Pedro Fernandes" 
         content="blablabla"
       />
-    </>
+    </div>
   )
 }
