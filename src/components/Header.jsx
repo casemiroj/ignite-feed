986b2907dc3d97d641d51c53daba0b7946e26218
João Casemiro <jv.casemiro@icloud.com>
2022-07-04 20:52:15 -0300

    Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import IgniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
   return(
-    <header className={styles.header}> 
-      <strong>Ignite Feed</strong>
+    <header className={styles.header}>
+      <img src={IgniteLogo} alt="Logotipo Ignite" /> 
     </header>
   )
 }
