30118956cf2e33acedde538ddfa636d56cc92cb5
João Casemiro <jv.casemiro@icloud.com>
2022-07-05 15:16:42 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar"
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="João Casemiro" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti distinctio beatae ducimus, recusandae eum impedit dicta earum doloremque, culpa at saepe voluptatem? Ipsum dolor dolores, temporibus sunt officiis accusantium quod?"
-        />
-      <Post 
-        author="Pedro Fernandes" 
-        content="blablabla"
-      />
+     
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+          author="João Casemiro" 
+          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti distinctio beatae ducimus, recusandae eum impedit dicta earum doloremque, culpa at saepe voluptatem? Ipsum dolor dolores, temporibus sunt officiis accusantium quod?"
+          />
+          <Post 
+            author="Pedro Fernandes" 
+            content="blablabla"
+          />
+        </main>
+      </div>
     </div>
   )
 }
