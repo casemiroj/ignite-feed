daa398cf13ffa27023756aaf7f43a4f74ce40ae1
João Casemiro <jv.casemiro@icloud.com>
2022-07-05 16:06:19 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -8,12 +10,21 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
       />
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://github.com/casemiroj.png"
+        />
+
         <strong>João Casemiro </strong>
         <span>Web Developer</span>
-        <footer>
-          <a href="#">Editar seu perfil</a>
-        </footer>
       </div>
+      
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
     </aside>
   )
 }
